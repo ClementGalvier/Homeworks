e8a0694872cc600a2f38e5e47276e0dc99e9ac48
ClementGalvierGitpersonal <105963254+ClementGalvierGitHub@users.noreply.github.com>
2022-07-16 15:26:02 +0800

    commit

--- Practise files/SQL/SELECT data.gender, data.age.sql
+++ Practise files/SQL/SELECT data.gender, data.age.sql
@@ -6,6 +6,7 @@ HAVING
 ORDER BY  gender, age -- by default by ASC but can specifyc DESC 
 LIMIT 10 --  return x amount of rows
 
+
 -- WILDCARDS 
 WHERE name LIKE 'b%' -- LIKE combined with the special character makes it powerful. whateever starts with 'b'
 WHERE name LIKE '%b' -- whatever ends with letter 'b'
@@ -13,6 +14,49 @@ WHERE name LIKE '%b%' -- whatever contains  letter 'b'
 
 WHERE name LIKE '%_o%' -- whatever has letter 'o' as 2nd character
 
+
+--------------------------------------
+
+SELECT DISTINCT name, points_game,-- what is contained in your display
+FROM bigquery-public-data.ncaa_basketball.mbb_historical_teams_games
+WHERE points_game > 163 AND name is not null
+ORDER BY points_game DESC
+LIMIT 10
+
+----------------
+SELECT name,win,points_game -- we dont need to include attendance to filter it
+FROM bigquery-public-data.ncaa_basketball.mbb_historical_teams_games
+WHERE points_game >150 OR attendance >77612
+LIMIT 10
+
+----------------
+SELECT games.game_id, mascots.name
+FROM bigquery-public-data.ncaa_basketball.mbb_games_sr games -- dont need the "AS"
+LEFT JOIN bigquery-public-data.ncaa_basketball.mascots mascots -- dont need the "AS"
+ON games.game_id = mascots.id
+WHERE mascots.mascot_name LIKE '%h%'
+LIMIT 5
+
+
+-----------
+SELECT DISTINCT mascots.name, AVG (games.attendance) AS average_attendance_session
+FROM bigquery-public-data.ncaa_basketball.mascots mascots
+JOIN bigquery-public-data.ncaa_basketball.mbb_games_sr games
+ON mascots.id =  games.h_id
+
+GROUP BY mascots.name
+ORDER BY average_attendance_session DESC
+
+--------------
+
+SELECT games.season season, AVG (games.attendance) AS average_attendance_session
+FROM bigquery-public-data.ncaa_basketball.mascots mascots
+JOIN bigquery-public-data.ncaa_basketball.mbb_games_sr games
+ON mascots.id =  games.h_id
+WHERE season >2014 -- WHERE comes first for the filter
+GROUP BY season -- your pivot table. only works with aggregation ( SUM, COUNT, AVG ...)
+HAVING average_attendance_session > 4900 -- only works with GROUP BY
+ORDER BY average_attendance_session DESC
 
 
 
